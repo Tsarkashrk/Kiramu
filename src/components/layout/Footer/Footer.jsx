785590c7b207d568fad768d73428040f15a8b752
Alisher Tokesh <tokesh04@mail.ru>
2023-09-04 00:10:46 +0600

    [Kiramu-9]: added footer section

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -4,10 +4,18 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='footer__contacts'>
-        <ion-icon name='logo-github'></ion-icon>
-        <ion-icon name='logo-linkedin'></ion-icon>
-        <ion-icon name='logo-instagram'></ion-icon>
-        <ion-icon name='paper-plane'></ion-icon>
+        <a href="https://github.com/Tsarkashrk" className="footer__link">
+          <ion-icon name='logo-github'></ion-icon>
+        </a>
+        <a href="https://www.linkedin.com/in/tsarka/" className="footer__link">
+          <ion-icon name='logo-linkedin'></ion-icon>
+        </a>
+        <a href="https://www.instagram.com/tsarka.shrk/" className="footer__link">
+          <ion-icon name='logo-instagram'></ion-icon>
+        </a>
+        <a href="https://t.me/tsarkagmshrk" className="footer__link">
+          <ion-icon name='paper-plane'></ion-icon>
+        </a>
       </div>
     </footer>
   )
