8f221f1d3df71179482621f87abc8b4a85dd6e37
Alisher Tokesh <tokesh04@mail.ru>
2023-09-06 21:15:49 +0600

    [Kiramu-14]: completed footer section

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -3,19 +3,37 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer'>
+      <div className='footer__project'>
+        <h2 className='footer__logo'>Kiramu</h2>
+        <div className='footer__author'>
+          © Copyright Danime 2023
+          <br></br>
+          Developed by: Green Line Studio
+        </div>
+      </div>
+
       <div className='footer__contacts'>
-        <a className="footer__link" href="https://github.com/Tsarkashrk" target='_blank'>
-          <ion-icon name='logo-github'></ion-icon>
-        </a>
-        <a className="footer__link" href="https://www.linkedin.com/in/tsarka/" target='_blank'>
-          <ion-icon name='logo-linkedin'></ion-icon>
-        </a>
-        <a className="footer__link" href="https://www.instagram.com/tsarka.shrk/" target='_blank'>
-          <ion-icon name='logo-instagram'></ion-icon>
-        </a>
-        <a className="footer__link" href="https://t.me/tsarkagmshrk" target='_blank'>
-          <ion-icon name='paper-plane'></ion-icon>
-        </a>
+        <div className='footer__links'>
+          <a className="footer__link" href="https://github.com/Tsarkashrk" target='_blank'>
+            <ion-icon name='logo-github'></ion-icon>
+          </a>
+          <a className="footer__link" href="https://www.linkedin.com/in/tsarka/" target='_blank'>
+            <ion-icon name='logo-linkedin'></ion-icon>
+          </a>
+          <a className="footer__link" href="https://www.instagram.com/tsarka.shrk/" target='_blank'>
+            <ion-icon name='logo-instagram'></ion-icon>
+          </a>
+          <a className="footer__link" href="https://t.me/tsarkagmshrk" target='_blank'>
+            <ion-icon name='paper-plane'></ion-icon>
+          </a>
+        </div>
+      </div>
+
+      <div className='footer__rights'>
+        <h2 className='footer__title'>Права</h2>
+        <p className='footer__description'>
+          Весь материал на сайте предназначен исключительно для бесплатного домашнего ознакомительного просмотра.
+        </p>
       </div>
     </footer>
   )
