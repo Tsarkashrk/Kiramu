efe9fde01d51df1a3371364af48e27e9a7caf76e
tsarka <tokesh04@mail.ru>
2024-03-02 13:30:45 +0600

    changes

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://kiramu-anime.vercel.app'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }),
